0978ece632e8bb2e7fdcd626e7c6fb82b356b112
wazaglo <wazaglo87@gmail.com>
2026-09-04 07:58:20 +0000

    chore: trigger synchronize for e2e

--- e2e_review_target.py
+++ e2e_review_target.py
@@ -7,3 +7,5 @@ STRIPE_SECRET_KEY = "sk_live_test_0000000000000000000000"
 def fetch_user(cursor, user_id):
     cursor.execute("SELECT * FROM users WHERE id = " + str(user_id))
     return cursor.fetchone()
+
+# e2e: synchronize trigger
